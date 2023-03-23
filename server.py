b1afba4dbe4a698bda0f6c1d70fc181c66400dcd
taserbeat <taserbeat@gmail.com>
2023-03-23 23:25:35 +0900

    feat: load configs from file and environment variables

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from concurrent.futures import ThreadPoolExecutor
 
+from core.settings import AppSettings
 from workers.cron_worker import CronWorker
 from workers.cron_task_option import CronTaskOption
 
@@ -14,27 +15,13 @@ def index(path):
 
 
 if __name__ == "__main__":
-    option1 = CronTaskOption.from_dict(
-        {
-            "TaskName": "SampleTask1",
-            "Command": "echo Task1; pwd;",
-            "Cron": "*/1 * * * *",
-            "Cwd": "./settings",
-        }
-    )
-    option2 = CronTaskOption.from_dict(
-        {
-            "TaskName": "SampleTask2",
-            "Command": "echo Task2; pwd;",
-            "Cron": "*/2 * * * *",
-            "Cwd": "./tests",
-        }
-    )
+    settings = AppSettings().load_setting_file().load_environment()
 
-    cron_worker = CronWorker(options=[option1, option2])
+    options = [CronTaskOption.from_dict(data) for data in settings.option_dicts]
+    cron_worker = CronWorker(options=options)
 
     with ThreadPoolExecutor(max_workers=1) as executor:
         executor.submit(cron_worker.start)
-        service.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+        service.run(host=settings.host, port=settings.port, debug=settings.debug, use_reloader=False)
 
     exit(0)
